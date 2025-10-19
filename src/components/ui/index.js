610fce0c1c7fa5d9432a7340fddd7b0caebe7148
MuhammadRamzan-bit <sp23-bse-027@cuiatk.edu.pk>
2025-10-20 01:33:43 +0500

    Update index.js

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,2 @@
-// Barrel exports for UI components.
-// Intentionally missing the export for './Text' (students must add it).
-export { default as Button } from './Button';
-export { default as TextInput } from './TextInput';
-// export { default as Text } from './Text'; // <-- students will uncomment/add this
+export { View, Text, TextInput, Button } from 'react-native';
+
