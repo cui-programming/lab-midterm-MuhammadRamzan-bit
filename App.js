1509bc84467780f79aa4ee1e81810d94d2f91d25
MuhammadRamzan-bit <sp23-bse-027@cuiatk.edu.pk>
2025-10-20 01:35:35 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
-import { styles } from './src/styles/styles';
-import AboutMe from './src/components/custom/AboutMe';
-import TasbihList from './src/components/custom/TasbihList';
-// import TeacherMessage from './src/components/custom/TeacherMessage'; // Students create this
-import SearchAndAdd from './src/components/custom/SearchAndAdd';
-import { STUDENT_NAME, REG_NO } from './src/config/student';
+import { ScrollView, View } from 'react-native';
+import { styles } from './src/style/styles';
+
+import AboutMe from './src/component/custom/AboutMe';
+import TeacherMessage from './src/component/custom/TeacherMessage';
+import TasbihList from './src/component/custom/TasbihList';
+import SearchAndAdd from './src/component/custom/SearchAndAdd';
 
 export default function App() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <AboutMe name={STUDENT_NAME} regNo={REG_NO} />
-      {/* TODO: after creating the custom/TeacherMessage component, render it here */}
-      <TasbihList />
-      <SearchAndAdd />
+      <View style={styles.section}>
+        <AboutMe />
+      </View>
+
+      <View style={styles.section}>
+        <TeacherMessage />
+      </View>
+
+      <View style={styles.section}>
+        <TasbihList />
+      </View>
+
+      <View style={styles.section}>
+        <SearchAndAdd />
+      </View>
     </ScrollView>
   );
 }
+
+
+
