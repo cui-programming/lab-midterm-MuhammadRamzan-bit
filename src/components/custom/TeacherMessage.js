7f784c0ce392f56c07b04bb8cbe816265e04c450
MuhammadRamzan-bit <sp23-bse-027@cuiatk.edu.pk>
2025-10-20 01:36:54 +0500

    Update TeacherMessage.js

--- src/components/custom/TeacherMessage.js
+++ src/components/custom/TeacherMessage.js
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import { View, Text, Button } from '../ui';
-// import styles from '../../style/styles';
-
-// export default function TeacherMessage() {
-//   const handlePress = () => alert("Message sent to teacher!");
-
-//   return (
-//     <View style={styles.teacherBox}>
-//       <Text style={styles.teacherHeading}>Message from Teacher</Text>
-//       <Text style={styles.teacherText}>
-//         Dear student, make sure to complete all tasks before submission time.
-//       </Text>
-//       <Button title="Send Acknowledgment" onPress={handlePress} />
-//     </View>
-//   );
-// }
-
-
 import React from 'react';
 import { View, Text } from '../ui';
 import { styles } from '../../style/styles';
@@ -31,3 +12,4 @@ export default function TeacherMessage() {
     </View>
   );
 }
+
