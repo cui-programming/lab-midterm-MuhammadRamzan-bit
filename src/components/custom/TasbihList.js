4934864ff6a55f81df49a8a2c9bf1c4f6fd72798
MuhammadRamzan-bit <sp23-bse-027@cuiatk.edu.pk>
2025-10-20 12:59:39 +0500

    Update TasbihList.js

--- src/components/custom/TasbihList.js
+++ src/components/custom/TasbihList.js
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import { Text, Button } from '../ui';
 import { styles } from '../../style/styles';
 
-export default function TasbihList({ items, increment, decrement }) {
+export default function TasbihList({ items, onIncrement, onDecrement }) {
   return (
     <View style={styles.tasbihContainer}>
       <Text style={styles.teacherHeading}>Tasbih Counter</Text>
@@ -14,8 +14,8 @@ export default function TasbihList({ items, increment, decrement }) {
           <Text style={styles.tasbihCount}>{item.count}</Text>
 
           <View style={styles.buttonRow}>
-            <Button title="+" onPress={() => increment(item.id)} />
-            <Button title="-" onPress={() => decrement(item.id)} />
+            <Button title="+" onPress={() => onIncrement(item.id)} />
+            <Button title="-" onPress={() => onDecrement(item.id)} />
           </View>
         </View>
       ))}
